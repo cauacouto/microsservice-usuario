02cc31c3424e305aebca79bca8526a0675fa6b7d
cauacouto <cauacouto022@gmail.com>
2026-06-09 21:34:32 -0300

    Adiciona o MapperUsuario para conversão entre entidades e DTOs

---
+++ src/main/java/dev/couto/microsservice_user/Mapper/MapperUsuario.java
@@ -0,0 +1,17 @@
+package dev.couto.microsservice_user.Mapper;
+
+
+import dev.couto.microsservice_user.Dto.UsuarioRequestDto;
+import dev.couto.microsservice_user.Dto.UsuarioResponseDto;
+import dev.couto.microsservice_user.domin.Usuario;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface MapperUsuario {
+
+
+     UsuarioResponseDto toDto(Usuario usuario);
+
+    Usuario toEntity(UsuarioRequestDto dto);
+}
