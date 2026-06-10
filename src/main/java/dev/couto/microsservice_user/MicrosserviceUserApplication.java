3e481ed6fbc69f92ecefd49236e1c5ac0e9e170b
cauacouto <cauacouto022@gmail.com>
2026-06-09 21:34:41 -0300

    Cria a classe principal do microsserviço de usuário

---
+++ src/main/java/dev/couto/microsservice_user/MicrosserviceUserApplication.java
@@ -0,0 +1,13 @@
+package dev.couto.microsservice_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosserviceUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosserviceUserApplication.class, args);
+	}
+
+}
