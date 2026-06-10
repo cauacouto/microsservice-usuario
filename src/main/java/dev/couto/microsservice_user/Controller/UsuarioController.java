68b9a6e06a5892cd4607de1426315eb44f3a89bf
cauacouto <cauacouto022@gmail.com>
2026-06-10 08:58:30 -0300

    feat:Adiciona endpoints para listar, atualizar e deletar usuários no controlador

--- src/main/java/dev/couto/microsservice_user/Controller/UsuarioController.java
+++ src/main/java/dev/couto/microsservice_user/Controller/UsuarioController.java
@@ -4,12 +4,13 @@ import dev.couto.microsservice_user.Dto.UsuarioRequestDto;
 import dev.couto.microsservice_user.Dto.UsuarioResponseDto;
 import dev.couto.microsservice_user.Service.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/usuario")
@@ -24,4 +25,26 @@ public class UsuarioController {
         var request = usuarioService.salvarUsuario(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(request);
     }
+
+    @GetMapping
+    public ResponseEntity<Page<UsuarioResponseDto>> listar(Pageable pageable){
+        return ResponseEntity.ok(usuarioService.listaUsuarios(pageable));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<UsuarioResponseDto> atualizar(UsuarioRequestDto dto, @PathVariable UUID id){
+        var request = usuarioService.atualizar(dto,id);
+        return ResponseEntity.ok().body(request);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarUsuario(@PathVariable UUID id){
+        usuarioService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+
+
+
 }
