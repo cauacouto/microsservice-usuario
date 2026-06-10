565a154fdee69f907059be17a191e66e9c01ff80
cauacouto <cauacouto022@gmail.com>
2026-06-09 21:34:58 -0300

    Adiciona classe de testes para o microsserviço de usuário

---
+++ src/test/java/dev/couto/microsservice_user/MicrosserviceUserApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.couto.microsservice_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosserviceUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
