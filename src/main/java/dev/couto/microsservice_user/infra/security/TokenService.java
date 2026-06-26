43f1d5a8c0c2293419d790efc0e5d0d732370c39
cauacouto <cauacouto022@gmail.com>
2026-06-25 21:58:55 -0300

    feat: implement TokenService for JWT generation and verification

---
+++ src/main/java/dev/couto/microsservice_user/infra/security/TokenService.java
@@ -0,0 +1,60 @@
+package dev.couto.microsservice_user.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import dev.couto.microsservice_user.domin.Usuario;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(Usuario usuario){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT.create()
+                    .withIssuer("user-microsservice")
+                    .withSubject(usuario.getEmail())
+                    .withExpiresAt(genereteExpirationDate())
+                    .sign(algorithm);
+            return token;
+
+        }catch (JWTCreationException ex){
+            throw new RuntimeException("token invalido");
+
+        }
+    }
+
+
+    public String verifyToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("user-microsserve")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch (JWTVerificationException ex){
+            return null;
+
+        }
+    }
+
+
+    private Instant genereteExpirationDate(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-3:00"));
+    }
+
+
+
+
+}
